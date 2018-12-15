f5d081b7a5df3f07b639b96b7e51855d4086e71e
Rafael Alberto <rafael.alberto1703@gmail.com>
2018-12-14 22:49:19 -0200

    atualização

--- beerstore/src/main/java/br/com/api/beerstore/resource/BeerResource.java
+++ beerstore/src/main/java/br/com/api/beerstore/resource/BeerResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/beers")
 public class BeerResource {
