685f53863a31417af035f3afb468c464f58008b7
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-09-01 16:43:39 -0300

    finalizando terraform configuration

--- beerstore/terraform/security.tf
+++ beerstore/terraform/security.tf
@@ -9,3 +9,15 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["${var.my_public_ip}"]
   }
 }
+
+resource "aws_security_group" "database" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "rafaalberto-database"
+
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    self = true
+  }
+}
