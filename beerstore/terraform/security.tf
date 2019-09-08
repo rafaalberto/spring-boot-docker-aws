66ef9c910c42d27a3ff568d0eb7233171d2a2a00
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-09-08 14:25:42 -0300

    atualizacao

--- beerstore/terraform/security.tf
+++ beerstore/terraform/security.tf
@@ -32,3 +32,49 @@ resource "aws_security_group" "allow_outbound" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "cluster_communication" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "beerstore-cluster-communication"
+
+  ingress {
+    from_port = 2377
+    to_port = 2377
+    protocol = "tcp"
+    self = true
+  }
+
+  ingress {
+    from_port = 7946
+    to_port = 7946
+    protocol = "tcp"
+    self = true
+  }
+
+  ingress {
+    from_port = 7946
+    to_port = 7946
+    protocol = "udp"
+    self = true
+  }
+
+  ingress {
+    from_port = 4789
+    to_port = 4789
+    protocol = "udp"
+    self = true
+  }
+
+}
+
+resource "aws_security_group" "allow_portainer" {
+  vpc_id = "${aws_vpc.main.id}"
+  name = "beerstore-allow-portainer"
+
+  ingress {
+    from_port = 9000
+    to_port = 9000
+    protocol = "tcp"
+    cidr_blocks = ["${var.my_public_ip}"]
+  }
+}
